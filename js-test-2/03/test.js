a887ef510401d6d11b98e1e41828f28f569b133f
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-03-02 14:30:59 +0530

    test

---
+++ js-test-2/03/test.js
@@ -0,0 +1,27 @@
+// Q function mergeIntervals(intervals) {
+//     // Your code here
+// }
+
+
+// ans :  [
+//     { start: 1, end: 6 },
+//     { start: 8, end: 10 },
+//     { start: 15, end: 18 }
+// ];
+
+//Q Build a todo app, Use localstorage, Add todo, Delete Todo?
+
+function mergeIntervals(intervals){
+inputIntervals
+}
+const inputIntervals = [
+    { start: 1, end: 3 },
+    { start: 2, end: 6 },
+    { start: 8, end: 10 },
+    { start: 15, end: 18 }
+];
+
+const mergedIntervals = mergeIntervals(inputIntervals);
+console.log(mergedIntervals);
+
+
