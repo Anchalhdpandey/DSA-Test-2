7192208149e3f851537dbb46f795a202227d4c3d
Anchalhdpandey <152679483+Anchalhdpandey@users.noreply.github.com>
2024-03-09 14:39:29 +0530

    updates

--- js-test-2/03/test.js
+++ js-test-2/03/test.js
@@ -1,27 +1,50 @@
-// Q function mergeIntervals(intervals) {
-//     // Your code here
-// }
-
-
-// ans :  [
-//     { start: 1, end: 6 },
-//     { start: 8, end: 10 },
-//     { start: 15, end: 18 }
-// ];
-
-//Q Build a todo app, Use localstorage, Add todo, Delete Todo?
-
-function mergeIntervals(intervals){
-inputIntervals
+var selectedRow = null;
+function onFormSubmit() {
+  event.preventDefault();
+  var formData = readFormData();
+  if (selectedRow == null) {
+    insertNewRecord(formData);
+  } else {
+    updateRecord(formData);
+  }
+  resetForm();
 }
-const inputIntervals = [
-    { start: 1, end: 3 },
-    { start: 2, end: 6 },
-    { start: 8, end: 10 },
-    { start: 15, end: 18 }
-];
 
-const mergedIntervals = mergeIntervals(inputIntervals);
-console.log(mergedIntervals);
+// retrive Data
+function readFormData() {
+  var formData = {};
+  formData["todocode"] = document.getElementById("todocode").value;
+  return formData;
+}
 
+// insert data
+function insertNewRecord(data) {
+  var table = document
+    .getElementById("storelist")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(0);
+  var cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.todocode;
+  var cell2 = newRow.insertCell(1);
+  cell2.innerHTML = `<button onClick="onEdit(this)">Edit</button><button onClick="onDelete(this)">Delete</button>`;
+  selectedRow = null;
+}
+function onEdit(td) {
+  selectedRow = td.parentNode.parentNode;
+  document.getElementById("todocode").value = selectedRow.cells[0].innerHTML;
+}
 
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.todocode;
+}
+
+function onDelete(td) {
+  if (confirm("Do you want to delete this record")) {
+    var row = td.parentNode.parentNode;
+    document.getElementById("storelist").deleteRow(row.rowIndex);
+  }
+  resetForm();
+}
+function resetForm() {
+  document.getElementById("todocode").value = "";
+}
